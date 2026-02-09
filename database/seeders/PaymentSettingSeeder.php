06ff15d52c78b3d27febfd475b08d9c3cc16e527
officetoufiqur <officetoufiqur19@gmail.com>
2026-02-09 09:49:51 +0600

    change

--- database/seeders/PaymentSettingSeeder.php
+++ database/seeders/PaymentSettingSeeder.php
@@ -14,12 +14,12 @@ class PaymentSettingSeeder extends Seeder
     public function run(): void
     {
         PaymentSetting::insert([
-            'mollie_key' => "test_b38t7gVvjqFBz7sQM932N2SQpRTwNs",
+            'mollie_key' => "mollie_test_key",
             'mollie_webhook' => null,
             'mollie_status' => false,
-            'stripe_secret_key' => "pk_test_51SuTVg02CdQWhn8pscHlaKpViRisCPBmISpHB45w78jqVzoVlkqGE8w7V2kkzvNrgKYQIkxMG5TGPgWSCibRY3uS00d2tNxpqL",
-            'stripe_publishable_key' => "sk_test_51SuTVg02CdQWhn8pdckrtLHNfyaejXXDi2hvS5fJxv6LwV2pmWTJAK9AMaSH8lLb9qSdbpruHbUZvHACdFV0xFeq00e91lSsCD",
-            'stripe_webhook' => "whsec_cc2eb2c547437981186ea18efe0331b77ced5794cea61acbeabad89dda6fedd2",
+            'stripe_secret_key' => "stripe_test_secret_key",
+            'stripe_publishable_key' => "stripe_test_publishable_key",
+            'stripe_webhook' => "stripe_test_webhook",
             'stripe_status' => false,
             'created_at' => now(),
             'updated_at' => now()
